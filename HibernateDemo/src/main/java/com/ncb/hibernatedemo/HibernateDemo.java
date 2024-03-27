04daaddc4b26cca193d9c5c521c6f72c6a3500f0
ncanhbao <ncanhbao@gmail.com>
2024-03-27 15:31:25 +0700

    Khoi tao sale app

--- HibernateDemo/src/main/java/com/ncb/hibernatedemo/HibernateDemo.java
+++ HibernateDemo/src/main/java/com/ncb/hibernatedemo/HibernateDemo.java
@@ -16,22 +16,7 @@ import org.hibernate.Session;
 public class HibernateDemo {
 
     public static void main(String[] args) {
-//        try (Session s = HibernateUtils.getFactory().openSession()) {
-//            Query q = s.createQuery("FROM Product");
-//            List<Product> cates = q.getResultList();
-//
-//            cates.forEach(c -> System.out.println(c.getId() + "   " + c.getName()));
-//        }
-
-        try (Session s = HibernateUtils.getFactory().openSession()) {
-            String kq = "%iPhone%";
-            Query q = s.createQuery("FROM Product");
-            q.setParameter("iPhone 11");
-            //q.setParameter("loc", kq);
-            List<Product> cates = q.getResultList();
-
-            cates.forEach(c -> System.out.println(c.getId() + "   " + c.getName()));
-        }
+        System.out.print("hello");
     }
 
 }
---
+++ SaleApp/src/main/webapp/WEB-INF/pages/index.jsp
@@ -0,0 +1,25 @@
+<%-- 
+    Document   : index
+    Created on : Mar 27, 2024, 1:32:18 PM
+    Author     : admin
+--%>
+
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Trang chủ</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+        <ul>
+            <c:forEach items="${categories}" var="c">
+                <li>
+                    ${c.name}
+                </li>
+            </c:forEach>
+        </ul>
+    </body>
+</html>
